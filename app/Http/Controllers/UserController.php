52c5cd6557872c049527499f9e4702e6863c2f51
Ronald <113407642+RonaldEuse@users.noreply.github.com>
2024-09-04 15:33:40 -0400

    [UPDATE]

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $user = User::all();
+        return response()->json($user);
+    }
+
+    public function show($id): JsonResponse
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+        return response()->json($user);
+    }
+
+}
